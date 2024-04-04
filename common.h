afce7d08b503e92cf2e885588e8a4bba30b2f8d4
rani-salman <rani.salman2lau.edu>
2024-04-04 23:35:19 +0300

    fixed common

--- common.h
+++ common.h
@@ -1,5 +1,6 @@
-#ifndef COMMON_H
-#define COMMON_H
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
 
 #define SEQUENCE_LENGTH 1024
 
@@ -8,9 +9,10 @@
 #define INSERTION   (-1)
 #define DELETION    (-1)
 
-void nw_gpu0(unsigned char* sequence1_d, unsigned char* sequence2_d, int* scores_d, unsigned int numSequences);
+void nw_gpu0(unsigned char* sequence1_d, unsigned char* sequence2_d, int* scores_d, unsigned int numSequences, int* ref_d, int* ref_hv, int* cur);
 void nw_gpu1(unsigned char* sequence1_d, unsigned char* sequence2_d, int* scores_d, unsigned int numSequences);
 void nw_gpu2(unsigned char* sequence1_d, unsigned char* sequence2_d, int* scores_d, unsigned int numSequences);
 void nw_gpu3(unsigned char* sequence1_d, unsigned char* sequence2_d, int* scores_d, unsigned int numSequences);
 
 #endif
+
